ae3079672d9af31eef4942ee27056581aa8407d0
Igor Andrade <igor.andrade@arctouch.com>
2021-08-20 15:05:49 -0300

    test

--- gestures.js
+++ gestures.js
@@ -9,7 +9,6 @@ AFRAME.registerComponent("gesture-handler", {
     },
   
     init: function () {
-      // this.handleScale = this.handleScale.bind(this);
       this.handleRotation = this.handleRotation.bind(this);
   
       this.isVisible = false;
@@ -28,38 +27,18 @@ AFRAME.registerComponent("gesture-handler", {
     update: function () {
       if (this.data.enabled) {
         this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
-        this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
       } else {
         this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
-        this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
       }
     },
   
     remove: function () {
       this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
-      this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
     },
   
     handleRotation: function (event) {
       if (this.isVisible) {
-        // this.el.object3D.rotation.y += event.detail.positionChange.y * this.data.rotationFactor;
         this.el.object3D.rotation.y += event.detail.positionChange.x * this.data.rotationFactor;
-      }
-    },
-  
-    handleScale: function (event) {
-      if (this.isVisible) {
-        this.scaleFactor *=
-          1 + event.detail.spreadChange / event.detail.startSpread;
-  
-        this.scaleFactor = Math.min(
-          Math.max(this.scaleFactor, this.data.minScale),
-          this.data.maxScale
-        );
-  
-        this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-        this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-        this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
       }
     },
   });
